8567c953e0c0e2c019ef15f25b8834ea611103a3
gorkavillar <gorka@gorkavillar.com>
2023-06-23 13:17:13 +0200

    Tema 05 - Refactorizando el componente ImagePlaceholder

--- src/components/ImagePlaceholder.tsx
+++ src/components/ImagePlaceholder.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 interface IProps {
     src: string
@@ -8,7 +8,10 @@ interface IProps {
 const placeHolderUrl = "https://www.loginradius.com/blog/static/00a89fc56461ea1529439d89072c93f1/701ee/react.jpg"
 
 const ImagePlaceholder = ({ src, alt }: IProps) => {
-    const [imageUrl, setImageUrl] = useState(src)
+    const [imageUrl, setImageUrl] = useState("")
+    useEffect(() => {
+        setImageUrl(src)
+    }, [src])
     const handleError = () => {
         // A través de la ref -> cambiar la src -> IMPERATIVO
         setImageUrl(placeHolderUrl)

dd90b67864335a94f97d04e00dc5a379d9cf72db
gorkavillar <gorka@gorkavillar.com>
2023-06-23 13:15:30 +0200

    Tema 05 - Componente para carga de imágenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useEffect } from "react"
 import "./App.css"
-import ChangeEvent from "./components/ChangeEvent"
-import Formulario from "./components/Formulario"
+import ImagePlaceholder from "./components/ImagePlaceholder"
 
 function App() {
+    // useEffect(() => {
+    //     window.addEventListener("dblclick", () => {
+    //         console.log("Se ha hecho doble click")
+    //     })
+    //     return () => {
+    //         window.removeEventListener("dblclick", () => null)
+    //     }
+    // }, [])
     return (
-        <div>
-            {/* <ChangeEvent /> */}
-            <Formulario />
+        <div className="img-container">
+            <ImagePlaceholder
+                src="https://woguclimbing.com/wp-content/uploads/2017/11/Dan-Osman.jpg"
+                alt="dan"
+            />
+            <ImagePlaceholder
+                src="https://encrypdted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwuHUnSYntFRn8-dNVXrJZQZnegp2l9WwbcECUY6fdBoGlWcIa0-dFGNN7CggAFPhOR58&usqp=CAU"
+                alt="rpi"
+            />
+            <ImagePlaceholder
+                src="https://assets-prd.ignimgs.com/2021/08/05/final-fantasy-xvi-button-1628180674117.jpg"
+                alt="ffxvi"
+            />
         </div>
     )
 }
---
+++ src/components/ImagePlaceholder.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+interface IProps {
+    src: string
+    alt?: string
+}
+
+const placeHolderUrl = "https://www.loginradius.com/blog/static/00a89fc56461ea1529439d89072c93f1/701ee/react.jpg"
+
+const ImagePlaceholder = ({ src, alt }: IProps) => {
+    const [imageUrl, setImageUrl] = useState(src)
+    const handleError = () => {
+        // A través de la ref -> cambiar la src -> IMPERATIVO
+        setImageUrl(placeHolderUrl)
+    }
+    return <img onError={handleError} src={imageUrl} alt={alt || ""} />
+}
+
+export default ImagePlaceholder

a619e867e7f9059766486e4a7b2e74d2f73d0681
gorkavillar <gorka@gorkavillar.com>
2023-07-14 12:13:14 +0200

    Tema 13 - Build para diferentes entornos

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import "./App.scss"
 import Contador from "./views/Contador/Contador"
 
 function App() {
+    const entorno = import.meta.env.MODE
     return (
-        <Contador />
+        <>
+            <h3>{import.meta.env.VITE_APP_NAME}</h3>
+            <Contador />
+            <p>Entorno: {entorno}</p>
+            <p>API Url: {import.meta.env.VITE_API_URL}</p>
+        </>
     )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const getOutDir = (mode: string) => {
+  if (mode === "production") return "dist-prod"
+  if (mode === "stg") return "dist-stg"
+  return "dist"
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  build: {
+    outDir: getOutDir(mode)
+  }
+}))
